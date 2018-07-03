df57da36b6b95d330154c9ad18a562fc0a5f0ce3
Robyn Klopp <rklopp@gmail.com>
2018-07-03 13:01:26 -0700

    add basic player functionality

--- app/src/main/java/com/robynandcory/podfav/PodcastPlayer.java
+++ app/src/main/java/com/robynandcory/podfav/PodcastPlayer.java
@@ -1,8 +1,10 @@
 package com.robynandcory.podfav;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,13 +24,13 @@ public class PodcastPlayer extends AppCompatActivity {
         setContentView(R.layout.play_podcast);
 
         Intent receivePodcastIntent = getIntent();
-//check if the intent was received, otherwise use public static String
+        //check if the intent was received, otherwise use public static String
         if (null != receivePodcastIntent) {
             String podcastName = receivePodcastIntent.getStringExtra(KEY_PODCAST);
             String podcastRanking = receivePodcastIntent.getStringExtra(KEY_RANKING);
             int podcastRid = Integer.parseInt(receivePodcastIntent.getStringExtra(KEY_IMG));
 
-//Display the information from the Main Activity in the text views
+            //Display the information from the Main Activity in the text views
             TextView nameTextView = findViewById(R.id.podcastName);
             nameTextView.setText(podcastName);
 
@@ -37,7 +39,40 @@ public class PodcastPlayer extends AppCompatActivity {
 
             ImageView podcastImageView = findViewById(R.id.podcastImage);
             podcastImageView.setImageResource(podcastRid);
+
+            final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.elizabethjenningsgraham);
+
+
+            ImageView play = findViewById(R.id.play);
+            play.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mediaPlayer.start(); // no need to call prepare(); create() does that for you
+                }
+            });
+
+            ImageView pause = findViewById(R.id.pause);
+            pause.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            mediaPlayer.pause(); // no need to call prepare(); create() does that for you
+                                        }
+            });
+
         }
+
+
+
+
+//public class podcastPlayer extends Service implements MediaPlayer.OnPreparedListener {
+//            private static final String ACTION_PLAY = "com.example.action.Play";
+//            MediaPlayer mMediaPlayer = null;
+//
+//            public int onPlay
+//    //Add Media Player
+//
+//            podcastPlayer.release();
+//    podcastPlayer =null;
 
     }
 }
